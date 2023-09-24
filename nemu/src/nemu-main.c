c955c2b98d4f6978e507f9194c656d376fe75a9a
tracer-ics2023 <tracer@njuics.org>
2023-09-24 09:03:28 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:03:28 up  2:15,  0 users,  load average: 0.06, 0.03, 0.04

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -13,52 +13,52 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
-// #include <common.h>
+#include <common.h>
 
-// void init_monitor(int, char *[]);
-// void am_init_monitor();
-// void engine_start();
-// int is_exit_status_bad();
+void init_monitor(int, char *[]);
+void am_init_monitor();
+void engine_start();
+int is_exit_status_bad();
 
-// int main(int argc, char *argv[]) {
-//   /* Initialize the monitor. */
-// #ifdef CONFIG_TARGET_AM
-//   am_init_monitor();
-// #else
-//   init_monitor(argc, argv);
-// #endif
+int main(int argc, char *argv[]) {
+  /* Initialize the monitor. */
+#ifdef CONFIG_TARGET_AM
+  am_init_monitor();
+#else
+  init_monitor(argc, argv);
+#endif
 
-// //   /* Start engine. */
-//   engine_start();
+//   /* Start engine. */
+  engine_start();
 
-//   return is_exit_status_bad();
-// }
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include "monitor/sdb/sdb.h"
-
-
-int main() {
-    FILE *file = fopen("exprs", "r");
-    if (file == NULL) {
-        printf("无法打开文件，喵~\n");
-        return 1;
-    }
-    bool success;
-
-    char expression[65536];
-    int expected_result;
-    while (fscanf(file, "%d %[^\n]", &expected_result, expression) != EOF) {
-        int result = expr(expression, &success);
-        if (result == expected_result) {
-            printf("测试通过，喵~ 表达式: %s, 结果: %d\n", expression, result);
-        } else {
-            printf("测试失败，喵~ 表达式: %s, 期望: %d, 实际: %d\n", expression, expected_result, result);
-        }
-    }
-
-    fclose(file);
-    return 0;
+  return is_exit_status_bad();
 }
+
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include "monitor/sdb/sdb.h"
+
+
+// int main() {
+//     FILE *file = fopen("exprs", "r");
+//     if (file == NULL) {
+//         printf("无法打开文件，喵~\n");
+//         return 1;
+//     }
+//     bool success;
+
+//     char expression[65536];
+//     int expected_result;
+//     while (fscanf(file, "%d %[^\n]", &expected_result, expression) != EOF) {
+//         int result = expr(expression, &success);
+//         if (result == expected_result) {
+//             printf("测试通过，喵~ 表达式: %s, 结果: %d\n", expression, result);
+//         } else {
+//             printf("测试失败，喵~ 表达式: %s, 期望: %d, 实际: %d\n", expression, expected_result, result);
+//         }
+//     }
+
+//     fclose(file);
+//     return 0;
+// }
