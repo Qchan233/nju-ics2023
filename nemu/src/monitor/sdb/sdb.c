8c3f55ae84438288ae16596dc0eccaa1a1b45085
tracer-ics2023 <tracer@njuics.org>
2023-09-25 14:43:33 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:43:33 up  9:36,  0 users,  load average: 0.02, 0.01, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -26,6 +26,7 @@ static int is_batch_mode = false;
 void init_regex();
 void init_wp_pool();
 
+
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
   static char *line_read = NULL;
--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -17,11 +17,11 @@
 
 #define NR_WP 32
 
+#define EXPR_LEN 64
 typedef struct watchpoint {
   int NO;
   struct watchpoint *next;
-  char* expr[64];
-
+  char expr[EXPR_LEN];
 } WP;
 
 static WP wp_pool[NR_WP] = {};
@@ -29,17 +29,43 @@ static WP *head = NULL, *free_ = NULL;
 
 void init_wp_pool() {
   int i;
-  for (i = 0; i < NR_WP; i ++) {
+  for (i = 0; i < NR_WP; i++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
+    memset(wp_pool[i].expr, 0, EXPR_LEN);
   }
 
   head = NULL;
   free_ = wp_pool;
 }
 
-// WP* new_wp(){
+WP* new_wp(char* expr){
+  WP* wp = free_;
+  if (free_ == NULL){
+    Log("Run out of watch points");
+    return NULL;
+  }
 
-// }
-/* TODO: Implement the functionality of watchpoint */
+  wp->next = head;
+  memset(wp->expr, 0, EXPR_LEN);
+  strcpy(wp->expr, expr);
+  head = wp;
+  free_ = free_->next;
+  return wp;
+}
+
+void free_wp(int index){
+  WP** wp_ptr = &head;
+
+  while (*wp_ptr){
+    if ((*wp_ptr)->NO == index){
+      (*wp_ptr)->next = free_;
+      free_ = *wp_ptr;
+      *wp_ptr = (*wp_ptr)->next;
+      break;
+    }
+    wp_ptr = &(*wp_ptr)->next;
+  } 
+}
+
 
