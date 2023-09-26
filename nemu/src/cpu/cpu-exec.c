7cd82a8d99d1d5eca7477d211b23fb72f2701042
tracer-ics2023 <tracer@njuics.org>
2023-09-26 15:55:16 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:55:16 up 51 min,  0 users,  load average: 0.01, 0.03, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -36,14 +36,14 @@ void device_update();
 bool check_change();
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
+  if (check_change()){
+    nemu_state.state = NEMU_STOP;
+  }
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-  if (check_change()){
-    nemu_state.state = NEMU_STOP;
-  }
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
