783713fec031a1b33b4dbe68601ca7cb2a50bec4
tracer-ics2023 <tracer@njuics.org>
2023-09-29 13:49:31 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:49:31 up  4:09,  0 users,  load average: 0.01, 0.04, 0.00

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -23,7 +23,7 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_R, TYPE_I, TYPE_S, TYPE_B, TYPE_U, TYPE_J, 
+  TYPE_Reg, TYPE_I, TYPE_S, TYPE_B, TYPE_U, TYPE_J, 
   TYPE_N,  // none
 };
 
@@ -41,7 +41,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
   int rs2 = BITS(i, 24, 20);
   *rd     = BITS(i, 11, 7);
   switch (type) {
-    case TYPE_R: src1R(); src2R();         break;
+    case TYPE_Reg: src1R(); src2R();         break;
     case TYPE_I: src1R();          immI(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_B: src1R(); src2R(); immB(); break;
@@ -71,7 +71,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 + imm, 4, src2));
 
   
-  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , R, R(rd) = R(src1) + R(src2));
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , Reg, R(rd) = R(src1) + R(src2));
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
