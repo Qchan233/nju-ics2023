116db6bfb33207b8e9d5d8c2710f0be1469505cf
tracer-ics2023 <tracer@njuics.org>
2023-10-04 19:16:42 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:16:42 up  8:55,  0 users,  load average: 0.06, 0.08, 0.03

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,7 +51,7 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  Log("Reading %d bytes from %u", len, addr);
+  Log("Reading %d bytes: %#x", len, addr);
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -59,7 +59,7 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  Log("Writing %d bytes to %u", len, addr);
+  Log("Writing %d bytes: %#x", len, addr);
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
