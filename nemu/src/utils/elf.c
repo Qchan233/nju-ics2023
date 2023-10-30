1a4042e680288702afa9d592b0f8f896ccfbfef9
tracer-ics2023 <tracer@njuics.org>
2023-10-30 13:13:37 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:13:37 up  2:38,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -2,6 +2,14 @@
 
 FILE *elf_fp = NULL;
 
+struct interval
+/*Interval [start, end)*/
+{
+    word_t start;
+    word_t end;
+};
+
+
 void init_elf(const char *elf_file){
     if (elf_file != NULL){
         Log("Opening ELF %s", elf_file);
