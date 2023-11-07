6be833cb66b61de674f0315b16efadfab82af960
tracer-ics2023 <tracer@njuics.org>
2023-11-07 13:55:44 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:55:44 up  2:54,  0 users,  load average: 0.31, 0.18, 0.06

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -44,11 +44,10 @@ char* itoa(int num,char* str,int radix)
 int sprintf(char *out, const char *fmt, ...);
 #define BUFFER_SIZE 2048
 int printf(const char *fmt, ...) {
-  // char buffer[BUFFER_SIZE];
-  // size_t count = snprintf(buffer, BUFFER_SIZE, fmt);
-  // putstr(buffer);
-  // return count;
-  return 0;
+  char buffer[BUFFER_SIZE];
+  size_t count = snprintf(buffer, BUFFER_SIZE, fmt);
+  putstr(buffer);
+  return count;
 }
 #undef BUFFER_SIZE
 
