a70fc33615ca58390e0975b1c193a68eec235e5e
tracer-ics2023 <tracer@njuics.org>
2023-11-08 10:05:27 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:05:27 up  6:40,  0 users,  load average: 0.06, 0.04, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -7,7 +7,7 @@ void __am_timer_init() {
 int printf(const char *fmt, ...);
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   // printf("Reading Time\n");
-  uptime->us = io_read(AM_TIMER_UPTIME).us;
+  // uptime->us = io_read(AM_TIMER_UPTIME).us;
   // panic("Not implmented");
 }
 
