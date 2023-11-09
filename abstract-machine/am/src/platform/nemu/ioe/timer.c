a240dc51d063428cfe9e6d46ecd7ad9df64cd8b2
tracer-ics2023 <tracer@njuics.org>
2023-11-09 10:34:09 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:34:09 up 14:47,  0 users,  load average: 0.00, 0.01, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -2,18 +2,18 @@
 #include <nemu.h>
 #include <time.h>
 
-static uint64_t base_time;
+// static uint64_t base_time;
 void __am_timer_init() {
-  uint32_t lo = inl(RTC_ADDR);
-  uint32_t hi = inl(RTC_ADDR + 4);
-  base_time = ((uint64_t) hi) << 32 | lo; 
+  // uint32_t lo = inl(RTC_ADDR);
+  // uint32_t hi = inl(RTC_ADDR + 4);
+  // base_time = ((uint64_t) hi) << 32 | lo; 
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint32_t lo = inl(RTC_ADDR);
   uint32_t hi = inl(RTC_ADDR + 4);
   uint64_t now = ((uint64_t) hi) << 32 | lo; 
-  uptime->us = now - base_time;
+  uptime->us = now;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
