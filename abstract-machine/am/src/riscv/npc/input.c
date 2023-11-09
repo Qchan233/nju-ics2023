9775395902fc8a77c69dfecbdc69e498dfbf51ab
tracer-ics2023 <tracer@njuics.org>
2023-11-09 15:12:02 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:12:02 up 19:25,  0 users,  load average: 0.03, 0.04, 0.01

--- abstract-machine/am/src/riscv/npc/input.c
+++ abstract-machine/am/src/riscv/npc/input.c
@@ -3,9 +3,9 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   kbd->keycode = inl(KBD_ADDR);
   if (kbd->keycode==AM_KEY_NONE){
-    kbd->keydown = 0;
+    kbd->keydown = false;
   }
   else{
-    kbd->keydown = 1;
+    kbd->keydown = true;
   }
 }
