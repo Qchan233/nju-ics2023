6cfea5acdf1143c35a5bcf740e7a98a8799159b3
tracer-ics2023 <tracer@njuics.org>
2023-11-10 19:33:07 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:33:07 up  5:57,  0 users,  load average: 0.26, 0.19, 0.14

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -35,7 +35,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int i,j;
   for(i=0;i<ctl->h;i++){
     for(j=0;j<ctl->w;j++){
-      outl(FB_ADDR + POS(ctl->x + i, ctl->y + j) * 4, ((uint32_t*)(ctl->pixels))[i*ctl->w + j]);
+      outl(FB_ADDR + POS(ctl->y + j, ctl->x + i) * 4, ((uint32_t*)(ctl->pixels))[i*ctl->w + j]);
     }
   }
   
