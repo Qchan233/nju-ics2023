05cda81cf0792e7fb51dba7a59d3c61e87575f10
tracer-ics2023 <tracer@njuics.org>
2023-11-10 20:55:10 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:55:10 up  7:19,  0 users,  load average: 0.13, 0.06, 0.06

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -16,7 +16,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = wh >> 16, .height = HMASK & wh,
-    .vmemsz = WIDTH * HEIGHT * 4
+    .vmemsz = 0
   };
 }
 
@@ -24,13 +24,12 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
-  // int i,j;
-  // for(j=0;j<ctl->h;j++){
-  //     for(i=0;i<ctl->w;i++){
-  //     outl(FB_ADDR + POS(ctl->x + i, ctl->y + j), ((uint32_t*)(ctl->pixels))[i + j * ctl->w]);
-  //   }
-  // }
-  
+  int i,j;
+  for(j=0;j<ctl->h;j++){
+      for(i=0;i<ctl->w;i++){
+      outl(FB_ADDR + POS(ctl->x + i, ctl->y + j), ((uint32_t*)(ctl->pixels))[i + j * ctl->w]);
+    }
+  }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
