e159a21eaa88954c1cb1b45dda87450ddcfa0da8
tracer-ics2023 <tracer@njuics.org>
2023-11-10 13:47:33 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:47:33 up 44 min,  0 users,  load average: 0.08, 0.11, 0.07

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -7,5 +7,5 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t kbd_info = inl(KBD_ADDR);
   kbd->keycode = kbd_info & ~KEYDOWN_MASK;
-  kbd->keydown = kbd_info - kbd->keycode;
+  kbd->keydown = kbd_info == kbd->keycode;
 }
