fe26170ea45a3274c58dc6de4e2f327a87cbb199
tracer-ics2023 <tracer@njuics.org>
2023-11-10 19:08:56 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:08:56 up  5:32,  0 users,  load average: 0.26, 0.24, 0.13

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -8,13 +8,13 @@ static int WIDTH, HEIGHT;
 void __am_gpu_init() {
   WIDTH = io_read(AM_GPU_CONFIG).width;
   HEIGHT = io_read(AM_GPU_CONFIG).height;
-  int i;
-  int w = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
-  int h = io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
+  // int i;
+  // int w = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
+  // int h = io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
   
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 1);
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w * h; i ++) fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 #define HMASK 0xFFFF
@@ -28,15 +28,14 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-
+  if (ctl->sync) {
+    outl(SYNC_ADDR, 1);
+  }
   int i,j;
   for(i=0;i<ctl->w;i++){
     for(j=0;j<ctl->h;j++){
       outl(FB_ADDR + POS(ctl->x + i, ctl->y + j) * 4, ((uint32_t*)(ctl->pixels))[i*ctl->w + j]);
     }
-  }
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
   }
   
 }
