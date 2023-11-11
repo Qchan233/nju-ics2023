c694f4f5ad48ac0248ff38bfd4a8df4ca77374a0
tracer-ics2023 <tracer@njuics.org>
2023-11-11 13:00:46 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:00:46 up 10:52,  0 users,  load average: 0.00, 0.03, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -8,19 +8,38 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
+static char* buffer_end;
 void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
   cfg->present = false;
+  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR); //in bytes
+  buffer_end = (char*) AUDIO_SBUF_ADDR + cfg->bufsize;
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  ctrl->channels = inl(AUDIO_CHANNELS_ADDR);
+  ctrl->freq = inl(AUDIO_FREQ_ADDR);
+  ctrl->samples = inl(AUDIO_SAMPLES_ADDR);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  uint32_t write_size = (char*) ctl->buf.end - (char*) ctl->buf.start;
+  AM_AUDIO_STATUS_T stat;
+  while (__am_audio_status(&stat), stat.count < write_size)
+  {
+    /*Wait for enough space*/
+  }
+  uint16_t* i;
+  uint16_t* write_pos = (uint16_t*) ((char*) buffer_end - stat.count);
+  for(i = (uint16_t*) ctl->buf.start; i < (uint16_t*)ctl->buf.end; i++){
+    outw((uintptr_t) write_pos, *i);
+    write_pos += 2;
+  }
+  
 }
