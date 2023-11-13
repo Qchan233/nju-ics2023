2ac31c7572f070c8765639441f16e18071cbeca9
tracer-ics2023 <tracer@njuics.org>
2023-11-13 11:11:30 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:11:30 up 1 day, 16:40,  0 users,  load average: 0.12, 0.05, 0.01

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -29,10 +29,13 @@ enum {
 
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
+static SDL_AudioSpec s = {};
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
-  
+
 }
+
+
 
 void init_audio() {
   uint32_t space_size = sizeof(uint32_t) * nr_reg;
@@ -45,4 +48,11 @@ void init_audio() {
 
   sbuf = (uint8_t *)new_space(CONFIG_SB_SIZE);
   add_mmio_map("audio-sbuf", CONFIG_SB_ADDR, sbuf, CONFIG_SB_SIZE, NULL);
+  
+  s.format = AUDIO_S16SYS;  // 假设系统中音频数据的格式总是使用16位有符号数来表示
+  s.userdata = NULL;        // 不使用
+
+  SDL_InitSubSystem(SDL_INIT_AUDIO);
+  SDL_OpenAudio(&s, NULL);
+  SDL_PauseAudio(0);
 }
