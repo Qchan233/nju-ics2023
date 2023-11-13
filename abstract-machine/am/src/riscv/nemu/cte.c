3a571ee17d4b462ea0b7564c8d751ce67a3dfd79
tracer-ics2023 <tracer@njuics.org>
2023-11-13 16:17:01 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:17:01 up 1 day, 21:46,  0 users,  load average: 0.08, 0.04, 0.01

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,9 +5,9 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-  printf("mepc: %u\n", c->mepc);
-  printf("mcause: %u\n", c->mcause);
-  printf("mstatus: %u\n", c->mstatus);
+  printf("mepc: %ll\n", c->mepc);
+  printf("mcause: %ll\n", c->mcause);
+  printf("mstatus: %ll\n", c->mstatus);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
