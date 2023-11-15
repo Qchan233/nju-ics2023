9985274ec51d6373212f9759af9c608d0b28e082
tracer-ics2023 <tracer@njuics.org>
2023-11-15 15:20:49 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:20:49 up 11:56,  0 users,  load average: 0.16, 0.04, 0.01

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -1,7 +1,7 @@
 #include <proc.h>
 
 #define MAX_NR_PROC 4
-
+void naive_uload(PCB *pcb, const char *filename) ;
 static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
@@ -22,7 +22,9 @@ void hello_fun(void *arg) {
 void init_proc() {
   switch_boot_pcb();
 
+
   Log("Initializing processes...");
+  naive_uload(NULL, NULL);
 
   // load program here
 }
