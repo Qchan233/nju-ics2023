8f63876f7e9a372804bea1dcc02d1925bef971ed
tracer-ics2023 <tracer@njuics.org>
2023-11-22 13:40:11 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:40:11 up  5:20,  0 users,  load average: 0.02, 0.06, 0.02

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,12 +5,15 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+#ifdef CONFIG_STRACE
   Log("System Call: %s", sysname[a[0]]);
-
+#endif
 
   switch (a[0]) {
     case SYS_yield: yield(); break;
     case SYS_exit: halt(0); break;
+    case SYS_write:
+      panic("SYS_write");
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
