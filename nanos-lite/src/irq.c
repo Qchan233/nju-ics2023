1b3521704516c24672d83b4475046dd272044ce8
tracer-ics2023 <tracer@njuics.org>
2023-11-22 12:52:30 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:52:30 up  4:32,  0 users,  load average: 0.00, 0.03, 0.00

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,7 +4,7 @@ void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:
-      Log("Yield");
+      Log("Yield\n");
       break;
     case EVENT_SYSCALL:
       do_syscall(c);
