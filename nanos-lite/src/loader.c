790e1a55c3a348a51fc1ee8b6d25d0937455fb92
tracer-ics2023 <tracer@njuics.org>
2023-11-23 15:18:49 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:18:49 up 14:26,  0 users,  load average: 0.11, 0.09, 0.06

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,15 +9,23 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
+
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   // TODO();
+  int fd = fs_open(filename, 0, 0);
   Elf_Ehdr ehdr;
-  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
   Elf_Phdr phdr[ehdr.e_phnum];
-  ramdisk_read(&phdr, ehdr.e_phoff, sizeof(Elf_Phdr) * ehdr.e_phnum);
+  fs_lseek(fd, ehdr.e_phoff, 0); //seek set
+  fs_read(fd, &phdr, sizeof(Elf_Phdr) * ehdr.e_phnum);
   int i;
   for(i=0;i<ehdr.e_phnum;i++){
     Elf_Phdr current = phdr[i];
