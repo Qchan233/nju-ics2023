6ae8720c4577e098c9b49670f3fbd51f929b85bd
tracer-ics2023 <tracer@njuics.org>
2023-11-23 17:11:02 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:11:02 up 16:19,  0 users,  load average: 0.01, 0.08, 0.04

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -56,16 +56,14 @@ int fs_open(const char *pathname, int flags, int mode){
 }
 
 size_t fs_read(int fd, void *buf, size_t len){
-  if (open_offsets[fd] > file_table[fd].disk_offset + file_table[fd].size){
-    printf("file_offsets= %d\n", open_offsets[fd]);
-  }
+  assert(open_offsets[fd] <= file_table[fd].disk_offset + file_table[fd].size);
   ramdisk_read(buf, open_offsets[fd], len);
   open_offsets[fd] += len;
   return len;
 }
 
 size_t fs_write(int fd, void *buf, size_t len){
-  if (open_offsets[fd] > file_table[fd].disk_offset + file_table[fd].size){
+  if (open_offsets[fd] <= file_table[fd].disk_offset + file_table[fd].size){
     printf("file_offsets= %d\n", open_offsets[fd]);
   }
   ramdisk_write(buf, open_offsets[fd], len);
