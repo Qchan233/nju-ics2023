d1f58ad4c56557a6f2d2203bf681920154909f18
tracer-ics2023 <tracer@njuics.org>
2023-11-23 15:31:26 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:31:26 up 14:39,  0 users,  load average: 0.10, 0.08, 0.05

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -11,7 +11,7 @@
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
-size_t ramdisk_copy(const void *buf, size_t offset, size_t len);
+size_t ramdisk_copy(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   // TODO();
