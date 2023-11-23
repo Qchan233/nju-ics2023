6b0b772e7e041465df6b9e374722b1607faec06c
tracer-ics2023 <tracer@njuics.org>
2023-11-23 13:12:38 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:12:38 up 12:20,  0 users,  load average: 0.05, 0.05, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -36,7 +36,6 @@ char* itoa(int num, char* str, int base)
     // only with base 10. Otherwise numbers are
     // considered unsigned.
     if (num < 0 && base == 10) {
-      putstr("here");
       if (num == INT32_MIN){
         strcpy(str, "-2147483648");
         return str;
@@ -46,6 +45,7 @@ char* itoa(int num, char* str, int base)
     }
     // Process individual digits
     while (num != 0) {
+        putstr("num = ");
         int rem = num % base;
         str[i++] = index[rem];
         num = num / base;
