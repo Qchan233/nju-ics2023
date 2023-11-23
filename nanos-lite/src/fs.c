60185d3cc33e5e8c6e244a1e4c0604117711c1e0
tracer-ics2023 <tracer@njuics.org>
2023-11-23 12:52:16 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:52:16 up 12:00,  0 users,  load average: 0.00, 0.00, 0.00

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -3,6 +3,8 @@
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
 typedef struct {
   char *name;
   size_t size;
@@ -31,6 +33,57 @@ static Finfo file_table[] __attribute__((used)) = {
 #include "files.h"
 };
 
+size_t *open_offsets;
+
 void init_fs() {
+  open_offsets = (size_t *)malloc(sizeof(size_t) * sizeof(file_table) / sizeof(file_table[0]));
   // TODO: initialize the size of /dev/fb
+}
+
+int fs_close(int fd){
+  return 0;
+}
+
+int fs_open(const char *pathname, int flags, int mode){
+  int i;
+  for (i = 0; i < sizeof(file_table) / sizeof(file_table[0]); i++){
+    if (strcmp(pathname, file_table[i].name) == 0){
+      open_offsets[i] = 0;
+      return i;
+    }
+  }
+  panic("No such file: %s", pathname);
+}
+
+size_t fs_read(int fd, void *buf, size_t len){
+  ramdisk_read(buf, file_table[fd].disk_offset, len);
+  open_offsets[fd] += len;
+  return len;
+}
+
+size_t fs_write(int fd, void *buf, size_t len){
+  ramdisk_write(buf, file_table[fd].disk_offset, len);
+  open_offsets[fd] += len;
+  return len;
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence){
+  switch (whence){
+    case SEEK_SET:
+      open_offsets[fd] = offset;
+      break;
+    case SEEK_CUR:
+      open_offsets[fd] += offset;
+      break;
+    case SEEK_END:
+      open_offsets[fd] = file_table[fd].size + offset;
+      break;
+    default:
+      panic("Invalid whence: %d", whence);
+  }
+
+  if (open_offsets[fd] > file_table[fd].size){
+    panic("Exceed file size: %d", file_table[fd].size);
+  }
+  return open_offsets[fd];
 }
