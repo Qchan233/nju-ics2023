1fcaff5ba7c1a456be0f3e66de1ad4086a5ee0bc
tracer-ics2023 <tracer@njuics.org>
2023-11-27 18:26:34 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:26:34 up  4:36,  0 users,  load average: 0.24, 0.16, 0.06

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -24,14 +24,13 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 
 size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-  printf("ev.keycode = %d\n", ev.keycode);
   if (ev.keycode == AM_KEY_NONE){
     return 0;
   }
   if (ev.keydown){
-    printf(buf, "kd %s", keyname[ev.keycode]);
+    sprintf(buf, "kd %s", keyname[ev.keycode]);
   } else {
-    printf(buf, "ku %s", keyname[ev.keycode]);
+    sprintf(buf, "ku %s", keyname[ev.keycode]);
   }
   return 3 + strlen(keyname[ev.keycode]);
 }
