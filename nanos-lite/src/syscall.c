22782ac52d6fb9d9e2b191fb3f2405dae271d498
tracer-ics2023 <tracer@njuics.org>
2023-11-27 18:20:53 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:20:53 up  4:30,  0 users,  load average: 0.20, 0.09, 0.02

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -58,7 +58,7 @@ void do_syscall(Context *c) {
     case SYS_brk:
       c->GPRx = 0;
       break;
-    case SYS_read: c->GPRx = fs_read((int) a[1], (void *)a[2], (size_t)a[3]); break;
+    case SYS_read: printf("read"); c->GPRx = fs_read((int) a[1], (void *)a[2], (size_t)a[3]); break;
     case SYS_close: c->GPRx = fs_close((int) a[1]); break;
     case SYS_lseek: c->GPRx = fs_lseek((int) a[1], (size_t) a[2], (int) a[3]); break;
     case SYS_gettimeofday: 
