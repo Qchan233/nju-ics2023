59aed44f2f410c08c59492cc92a89d2dceed13c2
tracer-ics2023 <tracer@njuics.org>
2023-11-27 18:25:29 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:25:29 up  4:35,  0 users,  load average: 0.25, 0.14, 0.05

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -27,6 +27,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   if (ev.keycode == AM_KEY_NONE){
     return 0;
   }
+  printf("keycode: %d\n", ev.keycode);
   if (ev.keydown){
     printf(buf, "kd %s", keyname[ev.keycode]);
   } else {
