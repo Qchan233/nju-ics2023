8a66d955c475344c177d97445a957ea277a670a7
tracer-ics2023 <tracer@njuics.org>
2023-11-27 18:09:35 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:09:35 up  4:19,  0 users,  load average: 0.09, 0.04, 0.01

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -32,7 +32,8 @@ Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
-  {"/dev/events", 0, 0, events_read, serial_write},
+  [FD_FB] =     {"/dev/fb", 0, 0, invalid_read, invalid_write},
+  [FD_EVENTS] = {"/dev/events", 0, 0, events_read, serial_write},
 #include "files.h"
 };
 
