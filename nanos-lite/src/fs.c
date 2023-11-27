6f14bd0968836e1fc6c04577a53716c3708a4122
tracer-ics2023 <tracer@njuics.org>
2023-11-27 18:01:45 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:01:45 up  4:11,  0 users,  load average: 0.08, 0.04, 0.00

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -6,6 +6,7 @@ typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t serial_write(const void *buf, size_t offset, size_t len);
+size_t events_read(void *buf, size_t offset, size_t len);
 typedef struct {
   char *name;
   size_t size;
@@ -31,6 +32,7 @@ Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
+  {"/dev/events", 0, 0, events_read, serial_write},
 #include "files.h"
 };
 
