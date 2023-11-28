15acc3c902ae242b78b2de8eede142b8c4f72d6c
tracer-ics2023 <tracer@njuics.org>
2023-11-28 11:18:22 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:18:22 up  6:18,  0 users,  load average: 0.14, 0.07, 0.01

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -41,7 +41,6 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   }
   AM_GPU_CONFIG_T info = io_read(AM_GPU_CONFIG);
   int w = info.width, h = info.height;
-  printf("dispinfo_read: w = %d, h = %d\n", w, h);
   memcpy(buf, &w, sizeof(int));
   memcpy(buf + sizeof(int), &h, sizeof(int));
   return sizeof(int) * 2;
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -8,6 +8,7 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t serial_write(const void *buf, size_t offset, size_t len);
 size_t events_read(void *buf, size_t offset, size_t len);
 size_t dispinfo_read(void *buf, size_t offset, size_t len);
+size_t fb_write(const void *buf, size_t offset, size_t len);
 typedef struct {
   char *name;
   size_t size;
@@ -16,7 +17,7 @@ typedef struct {
   WriteFn write;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO};
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -33,9 +34,9 @@ Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
-  [FD_FB] =     {"/dev/fb", 0, 0, invalid_read, invalid_write},
+  [FD_FB] =     {"/dev/fb", 0, 0, invalid_read, fb_write},
   [FD_EVENTS] = {"/dev/events", 0, 0, events_read, serial_write},
-  {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
+  [FD_DISPINFO] = {"/proc/dispinfo", 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
 };
 
@@ -48,9 +49,16 @@ ReadFn get_read_fn(int fd){
 }
 
 size_t *open_offsets;
-
+static int screen_width;
+static int screen_height;
 void init_fs() {
   open_offsets = (size_t *)malloc(sizeof(size_t) * sizeof(file_table) / sizeof(file_table[0]));
+  int buf[2];
+  dispinfo_read(buf, 0, 2 * sizeof(int));
+  screen_width = buf[0];
+  screen_height = buf[1];
+  file_table[FD_FB].size = screen_width * screen_height * sizeof(uint32_t);
+  printf("screen_width: %d, screen_height: %d\n", screen_width, screen_height);
 }
 
 int fs_close(int fd){
