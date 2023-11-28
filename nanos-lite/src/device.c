2678157704824aa8f820bca61476facdc459cf98
tracer-ics2023 <tracer@njuics.org>
2023-11-28 10:27:46 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:27:46 up  5:27,  0 users,  load average: 0.02, 0.04, 0.02

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -36,6 +36,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
+  printf("dispinfo_read\n");
   if (len < 2 * sizeof(int)){
     return 0;
   }
