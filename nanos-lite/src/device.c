bb1e8eab7b27f81ec53b551314a188f8ba69c29d
tracer-ics2023 <tracer@njuics.org>
2023-11-28 15:33:45 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:33:45 up 10:33,  0 users,  load average: 0.17, 0.16, 0.20

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -50,8 +50,8 @@ extern int screen_width;
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   int y = (offset / 4) / screen_width;
-  printf("y: %d\n", y);
   int x = (offset / 4) % screen_width;
+  printf("x:%d y: %d\n",x, y);
   void* line_buf = (void*)buf;
   io_write(AM_GPU_FBDRAW, x, y, line_buf, len / sizeof(uint32_t), 1, true);
   return 0;
