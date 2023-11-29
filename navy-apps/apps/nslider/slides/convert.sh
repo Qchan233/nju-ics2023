d8c1527edb826bab85dc876bd1c44037e6841766
tracer-ics2023 <tracer@njuics.org>
2023-11-29 18:26:02 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:26:02 up 21:39,  0 users,  load average: 0.20, 0.08, 0.02

--- navy-apps/apps/nslider/slides/convert.sh
+++ navy-apps/apps/nslider/slides/convert.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-convert slides.pdf \
-  -sharpen "0x1.0" \
-  -type truecolor -resize 400x300\! slides.bmp
+for i in 0 1 2 3 4 5
+do
+   convert "slides.pdf[$i]" -sharpen "0x1.0" -type truecolor -resize 400x300\! "slides-$i.bmp"
+done
 
 mkdir -p $NAVY_HOME/fsimg/share/slides/
 rm $NAVY_HOME/fsimg/share/slides/*
