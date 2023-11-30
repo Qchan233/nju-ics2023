50b7f17a78e61e43edb8fa7b4d840218ca8d2ead
tracer-ics2023 <tracer@njuics.org>
2023-11-30 12:25:31 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:25:31 up 1 day, 33 min,  0 users,  load average: 0.27, 0.19, 0.08

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -74,10 +74,10 @@ int main(int argc, char *argv[], char *envp[]) {
   font = new BDF_Font(font_fname);
   logo_sf = SDL_LoadBMP("/share/pictures/projectn.bmp");
   assert(logo_sf);
+  printf("1");
   set_i_max();
 
   while (1) {
-    printf("1");
     display_menu(i_max);
 
     SDL_Event e;
