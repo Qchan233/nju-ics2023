b3f95eea50a4791463a659800e84d72bb8045fa1
tracer-ics2023 <tracer@njuics.org>
2023-11-30 12:34:36 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:34:36 up 1 day, 42 min,  0 users,  load average: 0.11, 0.10, 0.08

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -32,7 +32,7 @@ void do_syscall(Context *c) {
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-  // Log("System Call: %s", sysname[a[0]]);
+  Log("System Call: %s", sysname[a[0]]);
 
   switch (a[0]) {
     case SYS_exit: halt(a[1]); break;
