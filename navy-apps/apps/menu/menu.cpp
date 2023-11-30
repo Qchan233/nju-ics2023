1d22ae7398479dd0085cb90ffb6eb2f668dc0b0e
tracer-ics2023 <tracer@njuics.org>
2023-11-30 12:24:35 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:24:35 up 1 day, 32 min,  0 users,  load average: 0.27, 0.17, 0.07

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -77,6 +77,7 @@ int main(int argc, char *argv[], char *envp[]) {
   set_i_max();
 
   while (1) {
+    printf("1");
     display_menu(i_max);
 
     SDL_Event e;
@@ -142,7 +143,6 @@ static void draw_text_row(char *s, int r) {
 static void display_menu(int n) {
   clear_display();
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
-  printf("1");
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
   printf("Available applications:\n");
   char buf[80];
