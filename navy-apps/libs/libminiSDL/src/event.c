2fe94371c7ae7498853dee0d6f43537a58c1c1d5
tracer-ics2023 <tracer@njuics.org>
2023-11-30 10:05:31 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:05:31 up 22:13,  0 users,  load average: 0.02, 0.02, 0.03

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -17,6 +17,19 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
+  char buf[16];
+  while(NDL_PollEvent(&buf, 0) == 0){
+    switch (buf[1])
+    {
+    case 'd':
+      event->type = SDL_KEYDOWN;
+      break;
+    case 'u':
+      event->type = SDL_KEYUP;
+    default:
+      break;
+    }
+  }
   return 1;
 }
 
