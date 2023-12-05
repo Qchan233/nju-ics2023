d8c835b7bd200e44fa2c46a671902952ae3756f6
tracer-ics2023 <tracer@njuics.org>
2023-12-05 16:52:13 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:52:13 up 1 day, 23:10,  0 users,  load average: 0.10, 0.11, 0.06

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -99,6 +99,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
   printf("SDL_UpdateRect\n");
+  printf("%d\n", s->format->BitsPerPixel);
   if (s->format->BitsPerPixel == 8){
     uint32_t* buf = (uint32_t *)malloc(sizeof(uint32_t)*w*h);
     int i;
