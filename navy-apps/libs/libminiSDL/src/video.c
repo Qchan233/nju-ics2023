977ad860b6cefc00539dccd73c1602f629a10472
tracer-ics2023 <tracer@njuics.org>
2023-12-06 13:23:23 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:23:23 up 2 days,  4:44,  0 users,  load average: 0.07, 0.06, 0.09

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -125,13 +125,12 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     if(x==0&&y==0&w==0&h==0){
       w = get_canvas_w();
       h = get_canvas_h();
-      printf("%d %d\n", w, h);
       NDL_DrawRect(buf, x, y, w, h);
       free(buf);
       return;
     }
-    printf("%p\n", buf);
     NDL_DrawRect(buf, x, y, w, h);
+    printf("drawn\n");
     free(buf);
     return;
   }
