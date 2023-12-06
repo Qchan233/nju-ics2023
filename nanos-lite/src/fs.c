c28a145ed44362c50f364f67b7912a3e7c2dcb9a
tracer-ics2023 <tracer@njuics.org>
2023-12-06 11:09:09 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:09:09 up 2 days,  2:30,  0 users,  load average: 0.47, 0.27, 0.20

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -68,6 +68,7 @@ int fs_close(int fd){
 int fs_open(const char *pathname, int flags, int mode){
   int i;
   for (i = 0; i < sizeof(file_table) / sizeof(file_table[0]); i++){
+    printf("file_table[%d].name: %s\n", i, file_table[i].name);
     if (strcmp(pathname, file_table[i].name) == 0){
       open_offsets[i] = file_table[i].disk_offset;
       return i;
