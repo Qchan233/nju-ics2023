5f1a298ae40d01bae8073abd7a3ca3e70da3e1f2
tracer-ics2023 <tracer@njuics.org>
2023-12-06 10:53:53 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:53:53 up 2 days,  2:15,  0 users,  load average: 0.30, 0.23, 0.11

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -25,7 +25,7 @@ static void sh_prompt() {
 static void sh_handle_cmd(const char *cmd) {
   char* cmd_copy = strdup(cmd);
   char *binary = strtok(cmd_copy, " ");
-  execvp(binary, NULL);
+  execve(binary, NULL, NULL);
 }
 
 void builtin_sh_run() {
