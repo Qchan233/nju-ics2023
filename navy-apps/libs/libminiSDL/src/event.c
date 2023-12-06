202285061a64002bfd9f6e91466eeaffcb63d496
tracer-ics2023 <tracer@njuics.org>
2023-12-06 14:54:04 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:54:04 up 2 days,  6:15,  0 users,  load average: 0.39, 0.25, 0.14

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -69,6 +69,8 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
 
 uint8_t keystates[83];
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  *numkeys = 83;
+  if (numkeys != NULL){
+    *numkeys = 83;
+  }
   return keystates;
 }
