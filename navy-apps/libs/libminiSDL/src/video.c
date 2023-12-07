da056c6ee1f318f9e7a667d23718426d38d2983d
tracer-ics2023 <tracer@njuics.org>
2023-12-07 11:17:24 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:17:24 up 2 days, 11:42,  0 users,  load average: 0.16, 0.10, 0.07

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -30,7 +30,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
       }
       return;
     }
-    printf("%d %d %d %d\n", dstx, dsty, srcrect->w, srcrect->h); 
+    // printf("%d %d %d %d\n", dstx, dsty, srcrect->w, srcrect->h); 
     for(i=0;i<srcrect->h;i++){
       for(j=0;j<srcrect->w;j++){
         dstpixel[(dsty+i)*dst->w + (dstx+j)] = srcpixel[(srcrect->y+i)*src->w + (srcrect->x+j)];
