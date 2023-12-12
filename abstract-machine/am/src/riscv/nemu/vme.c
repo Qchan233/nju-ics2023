2863293135f264b5fb334840806e4b5813df7180
tracer-ics2023 <tracer@njuics.org>
2023-12-12 15:22:56 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:22:56 up 13:37,  0 users,  load average: 0.07, 0.09, 0.03

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -72,5 +72,6 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
   Context* context = (kstack.end - sizeof(Context));
   context->mepc = (uintptr_t) entry;
+  context->GPRx = (uintptr_t) heap.end;
   return context;
 }
