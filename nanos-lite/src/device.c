da011a88e7b5c1c2ef57759a0f327021adfd8c79
tracer-ics2023 <tracer@njuics.org>
2023-12-12 14:25:23 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:25:23 up 12:40,  0 users,  load average: 0.16, 0.11, 0.04

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,6 +15,7 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
+  yield();
     int i;
     for (i = 0; i < len; i++){
       putch(((char *)buf)[i]);
@@ -23,6 +24,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
+  yield();
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if (ev.keycode == AM_KEY_NONE){
     sprintf(buf, "n");
@@ -50,6 +52,7 @@ extern int screen_height;
 extern int screen_width;
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
+  yield();
   int y = (offset / 4) / screen_width;
   int x = (offset / 4) % screen_width;
   io_write(AM_GPU_FBDRAW, x, y, (void*)buf, len / sizeof(uint32_t), 1, true);
