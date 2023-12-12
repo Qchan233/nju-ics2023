7e8048fcc9cdf2bd5a602b84079feb76ceae8e85
tracer-ics2023 <tracer@njuics.org>
2023-12-12 15:24:40 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:24:40 up 13:39,  0 users,  load average: 0.36, 0.23, 0.09

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -34,7 +34,7 @@ void init_proc() {
   Log("Initializing processes...");
   context_kload(&pcb[0], hello_fun, (void*) 0);
   // context_kload(&pcb[1], hello_fun, (void*) 1);
-  context_uload(&pcb[1], "/bin/pal");
+  context_uload(&pcb[0], "/bin/pal");
   assert(pcb[0].cp != NULL);
   assert(pcb[1].cp != NULL);
   switch_boot_pcb();
