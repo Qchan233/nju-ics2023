4087dd1e7c8ae214d21bfdfb0d32a8be57452698
tracer-ics2023 <tracer@njuics.org>
2023-12-12 17:04:26 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:04:26 up 15:19,  0 users,  load average: 0.00, 0.00, 0.00

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,6 +5,12 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
+  int argc = (int) *args;
+  char **argv = (char **)(args + 1);
+  char **envp = (char **)(args + argc + 2);
+
+  int i;
+
   char *empty[] =  {NULL };
   environ = empty;
   exit(main(0, empty, empty));
