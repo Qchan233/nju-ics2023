c3b36278308956f7543262690480402171cb00b1
tracer-ics2023 <tracer@njuics.org>
2023-12-13 11:21:54 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:21:54 up 18:20,  0 users,  load average: 0.09, 0.05, 0.01

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -23,18 +23,64 @@ void context_kload(PCB* thispcb, void (*func)(void *), void *arg){
     thispcb->cp = kcontext((Area) { thispcb->stack, thispcb->stack + STACK_SIZE}, func, arg);
 }
 
-
-void context_uload(PCB* thispcb, char* filename){
+#define BUFSIZE 16
+void context_uload(PCB *thispcb, const char *filename, char *const argv[], char *const envp[]){
     uintptr_t entry = naive_uload(thispcb, filename);
-    thispcb->cp = ucontext(NULL, (Area) { thispcb->stack, thispcb->stack + STACK_SIZE}, (void*)entry);
+    Context* context = ucontext(NULL, (Area) { thispcb->stack, thispcb->stack + STACK_SIZE}, (void*)entry);
+    thispcb->cp = context;
+
+    char* stack_top = (char*) context->GPRx;
+    int narg = 0;
+    int nenv = 0;
+    char* argbuf[BUFSIZE];
+    char* envbuf[BUFSIZE];
+
+    // 复制 argv 到栈上
+    while(argv[narg] != NULL){
+      stack_top -= strlen(argv[narg]) + 1;
+      strcpy(stack_top, argv[narg]);
+      argbuf[narg] = stack_top;
+      narg++;
+    }
+
+    // 复制 envp 到栈上
+    while(envp[nenv] != NULL){
+      stack_top -= strlen(envp[nenv]) + 1;
+      strcpy(stack_top, envp[nenv]);
+      envbuf[nenv] = stack_top;
+      nenv++;
+    }
+    
+    uintptr_t *stack_ptr = (uintptr_t*)((uintptr_t)stack_top & ~0x3);
+    stack_ptr--;
+
+    *stack_ptr = (uintptr_t) NULL;
+    int i;
+    for(i=0;i<nenv;i++){
+      *stack_ptr = (uintptr_t) envbuf[i];
+      stack_ptr--;
+    }
+
+    *stack_ptr = (uintptr_t) NULL;
+    stack_ptr--;
+
+    for(i=0;i<narg;i++){
+      *stack_ptr = (uintptr_t) argbuf[i];
+      stack_ptr--;
+    }
+    *stack_ptr = narg;
+  
 }
+#undef BUFSIZE
 
 
 void init_proc() {
   Log("Initializing processes...");
   context_kload(&pcb[0], hello_fun, (void*) 0);
   // context_kload(&pcb[1], hello_fun, (void*) 1);
-  context_uload(&pcb[1], "/bin/pal");
+  char* argc[] = {"--skip", NULL};
+  char* envp[] = {NULL};
+  context_uload(&pcb[1], "/bin/pal", argc, envp);
   assert(pcb[0].cp != NULL);
   assert(pcb[1].cp != NULL);
   switch_boot_pcb();
