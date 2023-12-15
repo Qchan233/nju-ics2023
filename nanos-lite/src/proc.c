bd426f966fa59894b1146447fd51a02e24540a98
tracer-ics2023 <tracer@njuics.org>
2023-12-15 11:13:15 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:13:15 up  8:50,  0 users,  load average: 0.20, 0.15, 0.07

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -99,6 +99,6 @@ Context* schedule(Context *prev) {
   current->cp = prev;
   current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   current_pcb = (current_pcb == 0 ? 1 : 0);
-  printf("current_pcb: %d\n", current_pcb);
+  // printf("current_pcb: %d\n", current_pcb);
   return current->cp;
 }
