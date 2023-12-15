02bcab781b5a5b86e1f2fa84e71bbc54add05e07
tracer-ics2023 <tracer@njuics.org>
2023-12-15 11:46:46 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:46:46 up  9:23,  0 users,  load average: 0.20, 0.06, 0.06

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,7 +15,7 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  putstr("Serial_write\n");
+  // putstr("Serial_write\n");
   yield();
     int i;
     for (i = 0; i < len; i++){
