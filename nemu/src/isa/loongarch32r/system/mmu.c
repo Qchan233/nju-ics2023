0a44111e8aa087014c8983fbe8eef5f222e4b876
tracer-ics2023 <tracer@njuics.org>
2023-12-20 17:06:56 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:06:56 up 1 day, 14:10,  0 users,  load average: 0.35, 0.20, 0.07

--- nemu/src/isa/loongarch32r/system/mmu.c
+++ nemu/src/isa/loongarch32r/system/mmu.c
@@ -19,9 +19,9 @@
 
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-  printf("%d,\n", paddr_read(cpu.satp, 4));
-  word_t vpn1 = (vaddr >> 22) & 0x3ff;
-  word_t vpn0 = (vaddr >> 12) & 0x3ff;
+  printf("sat_val: %d,\n", paddr_read(cpu.satp, 4));
+  // word_t vpn1 = (vaddr >> 22) & 0x3ff;
+  // word_t vpn0 = (vaddr >> 12) & 0x3ff;
 
   // word_t pte_val = pa
   return MEM_RET_FAIL;
