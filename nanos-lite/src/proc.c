8b82f356b8fb1bc4e60841b682175b0f48efa943
tracer-ics2023 <tracer@njuics.org>
2023-12-26 16:33:30 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:33:30 up 13:44,  0 users,  load average: 0.01, 0.07, 0.08

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -87,7 +87,7 @@ void init_proc() {
   // context_kload(&pcb[1], hello_fun, (void*) 1);
   char* argv[] = {"/bin/nterm", NULL};
   char* envp[] = {NULL};
-  context_uload(&pcb[1], "/bin/nterm", argv, envp);
+  context_uload(&pcb[1], "/bin/dummy", argv, envp);
   current_pcb = 1;
   assert(pcb[0].cp != NULL);
   assert(pcb[1].cp != NULL);
