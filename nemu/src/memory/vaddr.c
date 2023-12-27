75046910c6656d57c60f29ca19c58787da44f3b6
tracer-ics2023 <tracer@njuics.org>
2023-12-27 17:30:14 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:30:14 up 21:30,  0 users,  load average: 0.01, 0.04, 0.01

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -16,8 +16,10 @@
 #include <isa.h>
 #include <memory/paddr.h>
 
+word_t vaddr_read(vaddr_t addr, int len);
 word_t vaddr_ifetch(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  // return paddr_read(addr, len);
+  return vaddr_read(addr, len);
 }
 
 
