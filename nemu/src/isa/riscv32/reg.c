e63036df02ecce2a268996e927149a89d2b4dba8
tracer-ics2023 <tracer@njuics.org>
2023-12-27 16:13:35 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:13:35 up 20:13,  0 users,  load average: 0.07, 0.02, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -35,6 +35,8 @@ void isa_reg_display() {
   printf("%-6s: %#-10x %-10u\n", "mstatus", cpu.mstatus, cpu.mstatus);
   printf("%-6s: %#-10x %-10u\n", "mepc", cpu.mepc, cpu.mepc);
   printf("%-6s: %#-10x %-10u\n", "mtvec", cpu.mtvec, cpu.mtvec);
+  printf("%-6s: %#-10x %-10u\n", "satp", cpu.satp, cpu.satp);
+  
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
