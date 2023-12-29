ed4b95247395414de557c713f19f922a644ffb73
tracer-ics2023 <tracer@njuics.org>
2023-12-29 17:11:55 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:11:55 up  9:03,  0 users,  load average: 0.01, 0.04, 0.05

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -7,7 +7,6 @@ static void *pf = NULL;
 void* new_page(size_t nr_page) {
   void * prev_p = pf;
   pf += nr_page * PAGE_SIZE;
-  memset(prev_p, 0, nr_page * PAGE_SIZE);
   // printf("prev_page: %x next_page: %x\n", prev_p, pf);
   return prev_p;
 }
@@ -16,7 +15,7 @@ void* new_page(size_t nr_page) {
 static void* pg_alloc(int n) {
   int num_page = (n + PAGE_SIZE - 1) / PAGE_SIZE;
   void* pf = new_page(num_page);
-  memset(pf, 0, n); //
+  memset(pf, 0, n * PAGE_SIZE); //
   return pf;
 }
 #endif
