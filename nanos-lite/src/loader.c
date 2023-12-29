3dd6031171424fc0ae46a730f4cbba1aee7d011a
tracer-ics2023 <tracer@njuics.org>
2023-12-29 14:52:14 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:52:14 up  6:44,  0 users,  load average: 0.11, 0.05, 0.01

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -119,8 +119,9 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 uintptr_t naive_uload(PCB *pcb, const char *filename) {
   // printf("filename: %s\n", filename);
   uintptr_t entry = loader(pcb, filename);
+  printf("Address space: %p->%p\n", pcb->as.area.start, pcb->as.area.end);
   uint32_t stack_bottom = (uint32_t) pcb->as.area.end - 4 * PGSIZE;
-  printf("setting stack: %p -> %p\n", pcb->as.area.end, stack_bottom);
+  // printf("setting stack: %p -> %p\n", pcb->as.area.end, stack_bottom);
   set_vm_map(&pcb->as, (uintptr_t) stack_bottom, 4 * PGSIZE);
 
   // printf("filename: %s\n", filename);
