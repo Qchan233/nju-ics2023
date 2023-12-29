5d40cdadfddd871f263924235eb18bdc40b7bf69
tracer-ics2023 <tracer@njuics.org>
2023-12-29 19:07:46 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:07:46 up 14 min,  0 users,  load average: 0.25, 0.13, 0.09

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -31,7 +31,6 @@ void set_vm_map(AddrSpace* as, uintptr_t vaddr, size_t len){
   while(len > 0){
     uintptr_t vpn1 = (addr_pos >> 22) & 0x3ff;
     uintptr_t vpn0 = (addr_pos >> 12) & 0x3ff;
-    printf("vpn1: %x, vpn0: %x\n", vpn1, vpn0);
 
     uintptr_t page_addr; // second level page table
     if ((pdir[vpn1] & 1) == 0){ //check if the first level page table is valid
