8cf30fc4a06e3d6165bbe706f71d523ff26669b0
tracer-ics2023 <tracer@njuics.org>
2023-12-30 16:15:00 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:15:00 up 20:30,  0 users,  load average: 0.18, 0.12, 0.09

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,75 +26,9 @@ void context_kload(PCB* thispcb, void (*func)(void *), void *arg){
 
 void set_vm_map(AddrSpace* as, uintptr_t vaddr, size_t len);
 void map(AddrSpace *as, void *va, void *pa, int prot);
-#define BUFSIZE 16
-void context_uload(PCB *thispcb, const char *filename, char *const argv[], char *const envp[]){
-    protect(&thispcb->as);
-    Context* context = ucontext(&thispcb->as, (Area) { thispcb->stack, thispcb->stack + STACK_SIZE}, NULL);
-    thispcb->cp = context;
-
-    void* pstack_top = new_page(8); //低位
-    context->GPRx = (uintptr_t) (pstack_top + 8 * 4096);
-    char* stack_top = (char*) context->GPRx;
-    // printf("stack_top: %p\n", stack_top);
-    int narg = 0;
-    int nenv = 0;
-    char* argbuf[BUFSIZE];
-    char* envbuf[BUFSIZE];
-    // 复制 argv 到栈上
-    while(argv[narg] != NULL){
-      stack_top -= strlen(argv[narg]) + 1;
-      strcpy(stack_top, argv[narg]);
-      argbuf[narg] = stack_top;
-      narg++;
-    }
-if (envp == NULL)  goto envp_end;
-    // 复制 envp 到栈上
-    // printf("envp: %p\n", envp[0]);
-    while(envp[nenv] != NULL){
-      strcpy(stack_top, envp[nenv]);
-      stack_top -= strlen(envp[nenv]) + 1;
-      // printf("%s\n", envp[nenv]);
-      envbuf[nenv] = stack_top;
-      nenv++;
-    }
-envp_end:
-    uintptr_t *stack_ptr = (uintptr_t*)((uintptr_t)stack_top & ~0x3);
-    stack_ptr--;
-
-    *stack_ptr = (uintptr_t) NULL;
-    int i;
-    for(i=0;i<nenv;i++){
-      *stack_ptr = (uintptr_t) envbuf[nenv-i-1];
-      stack_ptr--;
-    }
-
-    *stack_ptr = (uintptr_t) NULL;
-    stack_ptr--;
-
-    for(i=0;i<narg;i++){
-      *stack_ptr = (uintptr_t) argbuf[narg-i-1];
-      stack_ptr--;
-    }
-    *stack_ptr = narg; 
-
-    // context->GPRx = (uintptr_t) stack_ptr;
-    // printf("Starting to load\n");
-    // TODO add stack map from va to pa
-    void* vstack_top = (void*) thispcb->as.area.end - 8 * PGSIZE;
-    int stack_i;
-    for(stack_i=0; stack_i< 8;stack_i++){
-      printf("mapping %p->%p\n", vstack_top + 4096 * stack_i, pstack_top + 4096 * stack_i);
-      map(&thispcb->as, vstack_top + 4096 * stack_i, pstack_top + 4096 * stack_i, 0 );
-    }
-
-    context->GPRx = (uintptr_t) thispcb->as.area.end;
-    
-
-    context->mepc = (uintptr_t) naive_uload(thispcb, filename);
-}
-#undef BUFSIZE
 
 
+void context_uload(PCB *thispcb, const char *filename, char *const argv[], char *const envp[]);
 void init_proc() {
   Log("Initializing processes...");
   context_kload(&pcb[0], hello_fun, (void*) 0);
