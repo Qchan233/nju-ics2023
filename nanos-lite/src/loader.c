52241afdb7d3b1f9daf2cb86c51e40ba6b36cf2e
tracer-ics2023 <tracer@njuics.org>
2024-01-02 10:40:51 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:40:51 up 1 day,  6:46,  0 users,  load average: 0.00, 0.00, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -186,6 +186,7 @@ envp_end:
     }
 
     context->GPRx = (uintptr_t) (pstack_top + 8 * 4096);
+    printf("pstack: %x", context->GPRx);
     
     context->mepc = (uintptr_t) naive_uload(thispcb, filename);
 
