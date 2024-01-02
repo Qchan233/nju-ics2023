d362017383b431962bafad476aee794ef9ca9778
tracer-ics2023 <tracer@njuics.org>
2024-01-02 12:12:01 +0800

    >  compile NEMU 3150105322 鱼少 Linux laptp 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:12:01 up 1 day,  8:18,  0 users,  load average: 0.03, 0.01, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -190,6 +190,7 @@ envp_end:
     context->GPRx = (uintptr_t) ((uint32_t)thispcb->as.area.end - stack_diff);
     // context->GPRx = (uintptr_t) stack_ptr;
     printf("vstack: %x\n", context->GPRx);
+    printf("mapped vstack: %x\n", get_addr(&thispcb->as, 0x7ffff9bc));
     
     context->mepc = (uintptr_t) naive_uload(thispcb, filename);
 }
